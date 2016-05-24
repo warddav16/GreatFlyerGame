a432d31abcbd154522704261e3332e2d3d847dfa
warddav16 <warddav16@gmail.com>
2016-05-24 18:12:43 -0500

    AI testing

---
+++ GreatFlyingGame/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ShipController))]
+public abstract class EnemyBase : MonoBehaviour
+{
+    private ShipController _shipController;
+    public float FiringThreshold = 10.0f;
+
+    void Awake()
+    {
+        _shipController = GetComponent<ShipController>();
+    }
+
+    void Update()
+    {
+        Player player = Player.Get();
+        Vector3 playerPos = player.transform.position;
+        Vector3 dirToPlayer = (playerPos - transform.position).normalized;
+
+        Quaternion dRot = Quaternion.LookRotation(dirToPlayer, transform.up);
+        Vector3 rot = (dRot * Quaternion.Inverse(transform.rotation)).eulerAngles;
+
+        _shipController.UpDown = rot.x;
+        _shipController.RightLeft = rot.y;
+
+        RaycastHit hitinfo;
+        bool shouldFire = false;
+        if( Physics.Raycast(transform.position, dirToPlayer, out hitinfo) )
+        {
+            if( hitinfo.transform.gameObject.tag == "Player")
+            {
+                if( Vector3.Dot(transform.forward, dirToPlayer) >= FiringThreshold)
+                {
+                    shouldFire = true;
+                }
+            }
+        }
+        _shipController.IsFirePrimary = shouldFire;
+    }
+}
