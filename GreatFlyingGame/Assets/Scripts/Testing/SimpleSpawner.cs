f0d1bd2601e184820f1c37f09af098f05d9685a5
warddav16 <warddav16@gmail.com>
2016-07-13 20:19:32 -0500

    Getting a simple spawner in there

---
+++ GreatFlyingGame/Assets/Scripts/Testing/SimpleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSpawner : MonoBehaviour
+{
+    public GameObject[] ObjectsToSpawn;
+    public Transform[] SpawnLocations;
+
+    public float SpawnRate = 1.0f;
+    private float _timer = 0;
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if( _timer >= SpawnRate )
+        {
+            GameObject objectToSpawn = ObjectsToSpawn[Random.Range(0, ObjectsToSpawn.Length)];
+            Transform loc = SpawnLocations[Random.Range(0, SpawnLocations.Length)];
+            Instantiate(objectToSpawn, loc.position, loc.rotation);
+            _timer = 0;
+        }
+    }
+}
