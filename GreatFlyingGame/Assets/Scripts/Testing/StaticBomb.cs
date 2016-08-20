a859bde6dddfe9c8dc7ccbb0e519acaf5fe67fb0
Alex Hollums <ahollums@gmail.com>
2016-08-20 10:01:07 -0400

    recommit prefabs bomb

---
+++ GreatFlyingGame/Assets/Scripts/Testing/StaticBomb.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticBomb : Health
+{
+    public float RadiusOfDoom = 40f;
+    public int DamageToDeal = 10;
+
+    public override void Kill()
+    {
+        Explode();
+        base.Kill();
+    }
+
+    void Explode()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, RadiusOfDoom);
+        foreach (Collider c in col)
+        {
+            Damageable<int> d = c.gameObject.GetComponent<Damageable<int>>();
+            if (d && d != this)
+            {
+                d.TakeDamage(DamageToDeal);
+            }
+        }
+    }
+
+    void Update()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, RadiusOfDoom);
+        foreach (Collider c in col)
+        {
+            if( c.gameObject == Player.Get().gameObject)
+            {
+                Kill();
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Color col = Gizmos.color;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, RadiusOfDoom);
+        Gizmos.color = col;
+    }
+}
